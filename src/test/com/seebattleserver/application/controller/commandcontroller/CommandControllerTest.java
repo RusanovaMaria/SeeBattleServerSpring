2f2d62e9f1c362a7534c8e64707f0e8ea09edca8
Rusanova Maria <mariarusanova1997@mail.ru>
2018-07-30 15:21:50 +0300

    Добавлены тесты к классу CommandController

--- src/test/com/seebattleserver/application/controller/commandcontroller/CommandControllerTest.java
+++ src/test/com/seebattleserver/application/controller/commandcontroller/CommandControllerTest.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.seebattleserver.application.controller.Controller;
 import com.seebattleserver.application.json.jsonmessage.JsonMessage;
 import com.seebattleserver.application.user.User;
+import com.seebattleserver.application.user.UserRegistry;
 import com.seebattleserver.service.sender.UserSender;
 import com.seebattleserver.service.sender.WebSocketUserSender;
 import org.junit.Before;
@@ -12,11 +13,11 @@ import org.mockito.Mock;
 import org.springframework.web.socket.TextMessage;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class CommandControllerTest {
-    private Controller controller;
+    private CommandController controller;
     private User user;
     private CommandList commandList;
     private Gson gson;
@@ -24,20 +25,46 @@ public class CommandControllerTest {
     @Mock
     private WebSocketUserSender userSender;
 
+    @Mock
+    private UserRegistry userRegistry;
+
     @Before
     public void setUp() {
         initMocks(this);
         user = mock(User.class);
-        commandList = mock(CommandList.class);
+        CommandList list = new CommandList(userRegistry);
+        commandList = spy(list);
         gson = new Gson();
         controller = new CommandController(user, commandList, userSender);
     }
 
     @Test
-    public void handle() {
+    public void handle_whenCommandIsCorrect_verifyGetCommandFromCommandListAndSendMessageToUser() {
         JsonMessage jsonMessage = new JsonMessage("help");
         String jsonString = gson.toJson(jsonMessage);
         TextMessage textMessage = new TextMessage(jsonString);
         controller.handle(textMessage);
+        verify(commandList).getCommand(jsonMessage.getContent());
+        verify(userSender).sendMessage(eq(user), any(JsonMessage.class));
+    }
+
+    @Test
+    public void handle_whenCommandIsNotCorrect_verifyGetDefaultCommandFromCommandListAndSendMessageToUser() {
+        JsonMessage jsonMessage = new JsonMessage("notCorrectCommand");
+        String jsonString = gson.toJson(jsonMessage);
+        TextMessage textMessage = new TextMessage(jsonString);
+        controller.handle(textMessage);
+        verify(commandList).getDefaultCommand();
+        verify(userSender).sendMessage(eq(user), any(JsonMessage.class));
+    }
+
+    @Test
+    public void handle_whenCommandIsNull_verifyGetDefaultCommandFromCommandListAndSendMessageToUser() {
+        JsonMessage jsonMessage = new JsonMessage(null);
+        String jsonString = gson.toJson(jsonMessage);
+        TextMessage textMessage = new TextMessage(jsonString);
+        controller.handle(textMessage);
+        verify(commandList).getDefaultCommand();
+        verify(userSender).sendMessage(eq(user), any(JsonMessage.class));
     }
 }
